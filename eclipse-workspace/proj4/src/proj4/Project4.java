850d9da95f4c3add8d71e1b37162e6dc00f7987d
density0 <hadaltundra@gmail.com>
2022-12-13 14:56:34 -0500

    Update Project4.java

--- eclipse-workspace/proj4/src/proj4/Project4.java
+++ eclipse-workspace/proj4/src/proj4/Project4.java
@@ -1,13 +1,17 @@
 package proj4;
 import java.util.Scanner;
-/**<p>Title: Math Problems </p>
- * <p>Description: This program generates random plus or minus math problems. </p>
+/**
+ * <p>Title: Math Questions  </p>
+ * <p>Description: This program creates a Question object and displays math questions the user can input and shows them if the answers they got were wrong or right, at the end tells
+ * them how many they got right and wrong and displays their percentage grade.</p>
  * @author Alesandel Lantigua
  */
+
 public class Project4 {
 
+
 	public static void main(String[] args)
-	{	
+	{
 		// instance variables
 		int addCorrectCount = 0;
 		int addIncorrectCount = 0;
@@ -15,21 +19,23 @@ public class Project4 {
 		int subIncorrectCount = 0;
 
 		Scanner scnr = new Scanner(System.in);
-		
-		// for loop for the number of randomly generated questions
-		for (int i = 1; i < 11; ++i) {
-			
+
+
+		for (int i = 1; i < 16; i++)
+		{		
 
 			// declares and instantiates a Question object
-			Question math = new Question();
-			
+			Question math = new Question();					
+
+
 			// prints the object's toString
 			System.out.println( "\nQ" + i + ": " + math.toString());
 
-			int input = scnr.nextInt();
+			// user input for the answer
+			int answer = scnr.nextInt();
 
 			// if else statement for if what the user puts is correct or not, counts how much they got wrong & right.
-			if (input == math.determineAnswer()) 
+			if (answer == math.determineAnswer()) 
 			{
 				System.out.println("Congrats!, you got it right c:"); 
 
@@ -39,9 +45,10 @@ public class Project4 {
 				if (math.getOperator() == '-') 
 					subCorrectCount++;
 			}
+
 			else
 			{
-				System.out.println("Dummy! its not " + input + ", it's " + math.determineAnswer());
+				System.out.println("wow did you really put? " + answer + ", im sorry to say this but it's " + math.determineAnswer());
 
 				if (math.getOperator() == '+')
 					addIncorrectCount++;
@@ -49,37 +56,40 @@ public class Project4 {
 				if (math.getOperator() == '-')
 					subIncorrectCount++;
 			}
-			
-			
-			// instance variables
-			int totalCorrect;
-			int grade;
-			
-			totalCorrect = addCorrectCount + subCorrectCount;
-			grade = totalCorrect * 10;
-			
-			// if else statement that tells you overall if you need to practice more or if you mastered the material.
-			if (grade >= 90)
-				System.out.println("\nCongratulations, you mastered basic math :D");
-			else
-				if (90 > grade && grade >= 75)
-					System.out.println("\nYour suppper close to fully understanding it, You can do it!");
-				else 
-					if (grade < 75)
-						System.out.println("\nyou need moooore practice");
-			
-			//prints how much you got wrong & right on addition problems
-			System.out.println("\nAddition:");
-			System.out.println("Correct:" + addCorrectCount);
-			System.out.println("Incorrect:" + addIncorrectCount);
+		}
 
-			//prints how much you got wrong & right on subtraction problems
-			System.out.println("\nSubtraction:");
-			System.out.println("Correct:" + subCorrectCount);
-			System.out.println("Incorrect:" + subIncorrectCount);
-			
-			//prints your percentage grade
-			System.out.println(grade + "%");
-		}	scnr.close();
-	} 
+
+		// instance variables
+		int totalCorrect;
+		double grade;
+
+		totalCorrect = addCorrectCount + subCorrectCount;
+		grade = (totalCorrect / 15.0) * 100.0;
+
+
+		// if else statement that tells you overall if you need to practice more or if you mastered the material.
+		if (grade >= 90)
+			System.out.println("\nCongratulations, you mastered basic math :D");
+		else
+			if (90 > grade && grade >= 75)
+				System.out.println("\nYour suppper close to fully understanding it, You can do it!");
+			else 
+				System.out.println("\nyou need moooore practice, maybe ask your mommy/daddy for help?");
+
+
+		//prints how much you got wrong & right on addition problems
+		System.out.println("\nAddition:");
+		System.out.println("Correct:" + addCorrectCount);
+		System.out.println("Incorrect:" + addIncorrectCount);
+
+		//prints how much you got wrong & right on subtraction problems
+		System.out.println("\nSubtraction:");
+		System.out.println("Correct:" + subCorrectCount);
+		System.out.println("Incorrect:" + subIncorrectCount);
+
+		//prints your percentage grade
+		System.out.println(grade + "%");
+		scnr.close();
+	}
+
 }
